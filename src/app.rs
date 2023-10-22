f932c28be0b95b2a8f800476f7908b168aae366c
Aitthi Arsa <meanstack20@gmail.com>
2023-10-22 20:02:45 +0700

    🎉 Begin a project.

---
+++ src/app.rs
@@ -0,0 +1,21 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+pub fn app() -> Html {
+    html! {
+            <main>
+                <img class="logo" src="/assets/image/assets-logo.png" alt="Yew logo" />
+                <div style="text-align: justify;">
+                    <span class="span-link"> {{"📦 Get in touch"}}</span>
+                </div>
+                <div align="left">
+                    <a href="https://github.com/AssetsArt" title="Github AssetsArt" style="margin-right: 10px;">
+                        <img src="https://img.shields.io/badge/github - AssetsArt-blue?logo=github&logoColor=white" alt="Github - AssetsArt" />
+                    </a>
+                    <a href="mailto:meanstack20+assets_art@gmail.com" title="Email Me">
+                        <img src="https://img.shields.io/badge/Email - meanstack20+assets_art@gmail.com-blue?logo=gmail&logoColor=white" alt=":meanstack20+assets_art@gmail.com" />
+                    </a>
+                </div>
+            </main>
+        }
+}
